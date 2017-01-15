4d5f293fed53fb6eccf8167d605b1fde7faaffbe
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2017-01-15 13:29:50 -0600

    * Initial Add

---
+++ include/DLWAssembly/DLWAssembly.h
@@ -0,0 +1,19 @@
+//
+//  DLWAssembly.h
+//  DLWAssembly
+//
+//  Created by Todd Ditchendorf on 15.01.17.
+//  Copyright © 2017 Celestial Teapot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DLWAssembly.
+FOUNDATION_EXPORT double DLWAssemblyVersionNumber;
+
+//! Project version string for DLWAssembly.
+FOUNDATION_EXPORT const unsigned char DLWAssemblyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DLWAssembly/PublicHeader.h>
+
+
