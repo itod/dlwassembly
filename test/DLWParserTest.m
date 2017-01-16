9aa2b2f8ddc4212a31bfb6641b9ef9144656952a
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2017-01-16 15:58:44 -0600

    * tests

--- test/DLWParserTest.m
+++ test/DLWParserTest.m
@@ -79,20 +79,7 @@
 }
 
 
-- (void)testAdd_1_2_A { [self testAddInstruction:@"add 1, 2, A;"]; }
-- (void)testAdd_A_B_C { [self testAddInstruction:@"add A, B, C;"]; }
-- (void)testAdd_A_1_C { [self testAddInstruction:@"add A, 1, C;"]; }
-- (void)testAdd_5_B_C { [self testAddInstruction:@"add 5, B, C;"]; }
-
-- (void)testSub_2_1_B { [self testSubInstruction:@"sub 2, 1, B;"]; }
-- (void)testSub_A_B_C { [self testSubInstruction:@"sub A, B, C;"]; }
-- (void)testSub_A_1_C { [self testSubInstruction:@"sub A, 1, C;"]; }
-- (void)testSub_5_B_C { [self testSubInstruction:@"sub 5, B, C;"]; }
-
-
-- (void)testLoadInstruction {
-    NSString *str = @"load #12, A;";
-    
+- (void)testLoadInstruction:(NSString *)str {
     NSError *err = nil;
     NSArray *prog = [p parseString:str error:&err];
     TDNil(err);
@@ -104,15 +91,13 @@
         DLWStatement *stmt = prog[0];
         TDEqualObjects([stmt class], [DLWLoadStatement class]);
         TDEquals(2, [stmt.children count]);
-        TDEqualObjects([stmt.children[0] class], [DLWAddressExpression class]);
+        TDTrue([stmt.children[0] isKindOfClass:[DLWExpression class]]);
         TDEqualObjects([stmt.children[1] class], [DLWRegisterDestination class]);
     }
 }
 
 
-- (void)testStoreInstruction {
-    NSString *str = @"store B, #42;";
-    
+- (void)testStoreInstruction:(NSString *)str {
     NSError *err = nil;
     NSArray *prog = [p parseString:str error:&err];
     TDNil(err);
@@ -124,9 +109,27 @@
         DLWStatement *stmt = prog[0];
         TDEqualObjects([stmt class], [DLWStoreStatement class]);
         TDEquals(2, [stmt.children count]);
-        TDEqualObjects([stmt.children[0] class], [DLWRegisterExpression class]);
+        TDTrue([stmt.children[0] isKindOfClass:[DLWExpression class]]);
         TDEqualObjects([stmt.children[1] class], [DLWMemoryDestination class]);
     }
 }
+
+
+- (void)testAdd_1_2_A { [self testAddInstruction:@"add 1, 2, A;"]; }
+- (void)testAdd_A_B_C { [self testAddInstruction:@"add A, B, C;"]; }
+- (void)testAdd_A_1_C { [self testAddInstruction:@"add A, 1, C;"]; }
+- (void)testAdd_5_B_C { [self testAddInstruction:@"add 5, B, C;"]; }
+
+- (void)testSub_2_1_B { [self testSubInstruction:@"sub 2, 1, B;"]; }
+- (void)testSub_A_B_C { [self testSubInstruction:@"sub A, B, C;"]; }
+- (void)testSub_A_1_C { [self testSubInstruction:@"sub A, 1, C;"]; }
+- (void)testSub_5_B_C { [self testSubInstruction:@"sub 5, B, C;"]; }
+
+
+//- (void)testLoad_12_A { [self testLoadInstruction:@"load 12, A;"]; }
+- (void)testLoad_a12_A { [self testLoadInstruction:@"load #12, A;"]; }
+//- (void)testLoad_rA_B { [self testLoadInstruction:@"load #A, B;"]; }
+
+- (void)testStoreInstruction { [self testStoreInstruction:@"store B, #42;"]; }
 
 @end
